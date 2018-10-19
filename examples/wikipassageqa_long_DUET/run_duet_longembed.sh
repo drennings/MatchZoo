1a9e17db46e62410cfb448dc32c1704a9882395c
drennings <daanrennings@gmail.com>
2018-10-19 17:05:59 +0200

    Adapted configs for long duet run

--- examples/wikipassageqa_long_DUET/run_duet_longembed.sh
+++ examples/wikipassageqa_long_DUET/run_duet_longembed.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_long/config/duet_wikipassageqa_longembed.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_long_DUET/config/duet_wikipassageqa_longembed.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_long/config/duet_wikipassageqa_longembed.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_long_DUET/config/duet_wikipassageqa_longembed.config
