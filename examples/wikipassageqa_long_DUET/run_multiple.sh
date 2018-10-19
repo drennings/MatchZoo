d807e01b13f9cb3561bf7727cfbb4e3283d05a29
Drennings <daanrennings@hotmail.com>
2018-10-19 14:25:14 +0200

    Pushing folder for DUET fix trial runs

---
+++ examples/wikipassageqa_long_DUET/run_multiple.sh
@@ -0,0 +1,14 @@
+#bash run_anmm.sh
+#bash run_drmm_tks.sh
+#bash run_duet.sh
+#bash run_matchpyramid.sh
+#bash run_mvlstm.sh
+#bash run_drmm.sh
+#bash run_cdssm.sh
+#bash run_dssm.sh # takes over 9 hours
+bash run_arci.sh
+bash run_arcii.sh
+bash run_knrm.sh
+bash run_conv_knrm.sh
+bash run_cdssm.sh
+
