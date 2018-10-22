34a7cb4062d338a64ee6040060001a6307352527
Drennings <daanrennings@hotmail.com>
2018-10-22 14:37:07 +0200

    Adapting filepaths for new TFC1 run

--- examples/wikipassageqa_tfc1_all/run_arcii.sh
+++ examples/wikipassageqa_tfc1_all/run_arcii.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_long/config/arcii_wikipassageqa.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/arcii_wikipassageqa.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_long/config/arcii_wikipassageqa.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/arcii_wikipassageqa.config
--- examples/wikipassageqa_tfc1_all/run_knrm.sh
+++ examples/wikipassageqa_tfc1_all/run_knrm.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_long/config/knrm_wikipassageqa.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/knrm_wikipassageqa.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_long/config/knrm_wikipassageqa.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/knrm_wikipassageqa.config
