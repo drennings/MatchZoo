d150b49127febd0e1f272d2955b76d490430465f
Drennings <daanrennings@hotmail.com>
2018-10-22 14:41:54 +0200

    Fixing typo

--- examples/wikipassageqa_tfc1_all/run_arci.sh
+++ examples/wikipassageqa_tfc1_all/run_arci.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/arci_wikipassageqa.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_tfc1_all/config/arci_wikipassageqa.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/arci_wikipassageqa.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_tfc1_all/config/arci_wikipassageqa.config
--- examples/wikipassageqa_tfc1_all/run_conv_knrm.sh
+++ examples/wikipassageqa_tfc1_all/run_conv_knrm.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/conv_knrm_wikipassageqa.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/wikipassageqa_tfc1_all/config/conv_knrm_wikipassageqa.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_tfc1_allg/config/conv_knrm_wikipassageqa.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/wikipassageqa_tfc1_all/config/conv_knrm_wikipassageqa.config
