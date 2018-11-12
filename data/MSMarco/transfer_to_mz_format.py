f8a6bbf191b8f87c522aab8ba922d7d23ed003ee
drennings <daanrennings@gmail.com>
2018-11-12 18:12:50 +0100

    Adapted method to do regex before other things

--- data/MSMarco/transfer_to_mz_format.py
+++ data/MSMarco/transfer_to_mz_format.py
@@ -2,8 +2,8 @@
 
 import os
 import sys
-
-
+import codecs
+import re
 basedir = './MSMarco/'
 dstdir = './'
 infiles = [ basedir + 'MSMarco-train-filtered.txt', basedir + 'MSMarco-dev-filtered.txt', basedir + 'MSMarco-test-filtered.txt' ]
@@ -11,9 +11,12 @@ outfiles = [ dstdir + 'MSMarco-mz-train.txt', dstdir + 'MSMarco-mz-dev.txt', dst
 
 for idx, infile in enumerate(infiles):
     outfile = outfiles[idx]
-    fout = open(outfile, 'w')
-    for line in open(infile, 'r'):
+    fout = open(outfile, 'w')#, "utf-8")
+    for line in open(infile, 'r'):#, "utf-8"):
+        #line = re.sub("[^A-Za-z0-9]+", " ", line)
         r = line.strip().split('\t')
+        for i in range(len(r)):
+            r[i] = re.sub("[^A-Za-z0-9]+", " ", r[i])
         fout.write('%s\t%s\t%s\n' % (r[2], r[0], r[1]))
     fout.close()
 
