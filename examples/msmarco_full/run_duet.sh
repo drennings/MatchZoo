15f2e566a3aa2f59cbe6e1e992b29af8d22aecfe
drennings <daanrennings@gmail.com>
2019-01-06 19:49:44 +0100

    Added gitignores and adapted configs for new full run

--- examples/msmarco_full/run_duet.sh
+++ examples/msmarco_full/run_duet.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/msmarco/config/duet_msmarco.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/msmarco_full/config/duet_msmarco.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/msmarco/config/duet_msmarco.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/msmarco_full/config/duet_msmarco.config
