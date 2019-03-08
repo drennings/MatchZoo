4bda511ad9aec4f09f4fb1f0b503e99d66d1bf95
drennings <daanrennings@gmail.com>
2019-03-08 18:04:47 +0100

    Created new folder for LNC2 test

---
+++ data/MSMarco_LNC2_TEST/transfer_to_mz_format.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+import os
+import sys
+import codecs
+import re
+basedir = './MSMarco/'
+dstdir = './'
+infiles = [ basedir + 'MSMarco-train-filtered.txt', basedir + 'MSMarco-dev-filtered.txt', basedir + 'MSMarco-test-filtered.txt' ]
+outfiles = [ dstdir + 'MSMarco-mz-train.txt', dstdir + 'MSMarco-mz-dev.txt', dstdir + 'MSMarco-mz-test.txt' ]
+
+#for idx, infile in enumerate(infiles):
+#    outfile = outfiles[idx]
+#    fout = open(outfile, 'w')#, "utf-8")
+#    for line in open(infile, 'r'):#, "utf-8"):
+#        #line = re.sub("[^A-Za-z0-9]+", " ", line)
+#        r = line.strip().split('\t')
+#        if len(r) != 3:
+#            print("HERE")
+#            print(r)
+#        for i in range(len(r)):
+#            r[i] = r[i].strip("\n").strip("\t")
+#        #for i in range(len(r)): #TODO rem0ved this
+#        #    r[i] = re.sub("[^A-Za-z0-9]+", " ", r[i])
+#        fout.write('%s\t%s\t%s\n' % (r[2], r[0], r[1]))
+#    fout.close()
+
+for idx, infile in enumerate(infiles):
+    outfile = outfiles[idx]
+    fout = open(outfile, 'w')#, "utf-8")
+    for line in open(infile, 'r'):#, "utf-8"):
+        #line = re.sub("[^A-Za-z0-9]+", " ", line)
+        r = line.strip().split('\t')
+        if len(r) != 5:
+            #print("HERE")
+            print(r)
+        for i in range(len(r)):
+            r[i] = r[i].strip("\n").strip("\t")
+        #for i in range(len(r)): #TODO rem0ved this
+        #    r[i] = re.sub("[^A-Za-z0-9]+", " ", r[i])
+        fout.write('Q%s\tD%s\t%s\t%s\t%s\n' % (r[0], r[1], r[2], r[3], r[4]))
+    fout.close()
+
+
